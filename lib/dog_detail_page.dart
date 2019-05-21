15362f6b483ab7e5338437a91c5078bea3db5ba5
Thomas Guyader <thomas.guyader@recif.nc>
2019-05-21 16:25:09 +1100

    feat(DogPage): ajout de la page de détail de dog

---
+++ lib/dog_detail_page.dart
@@ -0,0 +1,117 @@
+
+import 'package:flutter/material.dart';
+
+import 'dog_model.dart';
+
+class DogDetailPage extends StatefulWidget{
+  final Dog dog;
+
+  DogDetailPage(this.dog);
+
+  @override
+  State<StatefulWidget> createState() => _DogDetailPageState();
+
+}
+
+class _DogDetailPageState extends State<DogDetailPage>{
+  final double dogAvatarSize = 150.0;
+
+  Widget get dogImage {
+    return Container(
+      height: dogAvatarSize,
+      width: dogAvatarSize,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        boxShadow: [
+          const BoxShadow(
+            offset: const Offset(1.0, 2.0),
+            blurRadius: 2.0,
+            spreadRadius: -1.0,
+            color: const Color(0x33000000),
+          ),
+          const BoxShadow(
+            offset: const Offset(2.0, 1.0),
+            blurRadius: 3.0,
+            spreadRadius: 0.0,
+            color: const Color(0x24000000),
+          ),
+          const BoxShadow(
+            offset: const Offset(3.0, 2.0),
+            blurRadius: 4.0,
+            spreadRadius: 2.0,
+            color: const Color(0x1F000000),
+          ),
+        ],
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(widget.dog.imageUrl),
+        ),
+      ),
+    );
+  }
+
+  Widget get rating {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          Icons.star,
+          size: 40.0,
+        ),
+        Text(' ${widget.dog.rating} / 10',
+          style: Theme.of(context).textTheme.display2,
+        ),
+      ],
+    );
+  }
+
+  Widget get dogProfile {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 32.0),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          stops: [0.1, 0.5, 0.7, 0.9],
+          colors: [
+            Colors.indigo[800],
+            Colors.indigo[700],
+            Colors.indigo[600],
+            Colors.indigo[400],
+          ],
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          dogImage,
+          Text(
+            '${widget.dog.name}  🎾',
+            style: TextStyle(fontSize: 32.0),
+          ),
+          Text(
+            widget.dog.location,
+            style: TextStyle(fontSize: 20.0),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+            child: Text(widget.dog.description),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      appBar: AppBar(
+        backgroundColor: Colors.black87,
+        title: Text('Meet ${widget.dog.name}'),
+      ),
+      body: dogProfile,
+    );
+  }
+
+}
